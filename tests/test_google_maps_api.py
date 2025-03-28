f7fd1aeb9d896d9d57a06752e539a181e82a798c
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-28 11:40:32 +0300

    add test create new place

---
+++ tests/test_google_maps_api.py
@@ -0,0 +1,23 @@
+from utils.api import GoogleMapsApi
+import requests
+
+"""Создание, изменение и удаление новой локации"""
+class TestCreatePlace():
+
+    def test_create_new_place(self):
+
+        print("\nМетод POST")
+        result_post = GoogleMapsApi.create_new_place()
+
+        # place_id
+        check_post = result_post.json()
+        place_id = check_post.get('place_id')
+
+        print("\nМетод GET POST")
+        result_get = GoogleMapsApi.get_new_place(place_id)
+
+        print("\nМетод PUT")
+        result_put = GoogleMapsApi.put_new_place(place_id)
+
+        print("\nМетод GET PUT")
+        result_get = GoogleMapsApi.get_new_place(place_id)
