64a6b890a01f61b5dc9fef55ff23b2fa705ac009
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-28 11:38:53 +0300

    add http methods

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,2 +1,24 @@
 import requests
 
+
+class HttpMethods:
+    """Список HTTP методов"""
+
+    headers = {'Content-Type': 'application/json'}
+    cookie = ''
+
+    @classmethod
+    def get(cls, url):
+        return requests.get(url=url, headers=cls.headers, cookies=cls.cookie)
+
+    @classmethod
+    def post(cls, url, body):
+        return requests.post(url=url, headers=cls.headers, cookies=cls.cookie, json=body)
+
+    @classmethod
+    def put(cls, url, body):
+        return requests.put(url=url, headers=cls.headers, cookies=cls.cookie, json=body)
+
+    @classmethod
+    def delete(cls, url, body):
+        return requests.delete(url=url, headers=cls.headers, cookies=cls.cookie, json=body)
