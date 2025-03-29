480285d297a3d6ff8eae33bf53ecee497882d506
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-29 12:58:04 +0300

    add method check json token

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,36 +1,51 @@
 from utils.api import GoogleMapsApi
 from utils.checking import Checking
 import requests
+import json
 
 """Создание, изменение и удаление новой локации"""
 class TestCreatePlace():
 
     def test_create_new_place(self):
-
         print("\nМетод POST")
         result_post = GoogleMapsApi.create_new_place()
         Checking.check_status_code(result_post, 200)
-
-        # place_id
-        check_post = result_post.json()
-        place_id = check_post.get('place_id')
+        
+        place_id = result_post.json().get("place_id")   # place_id
+        token_post = self.get_list_token(result_post)
+        Checking.check_json_token(result_post, token_post)
 
         print("\nМетод GET POST")
         result_get = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        token_get = self.get_list_token(result_get)
+        Checking.check_json_token(result_get, token_get)
 
         print("\nМетод PUT")
         result_put = GoogleMapsApi.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        token_put = self.get_list_token(result_put)
+        Checking.check_json_token(result_put, token_put)
 
         print("\nМетод GET PUT")
         result_get = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        token_get = self.get_list_token(result_get)
+        Checking.check_json_token(result_get, token_get)
 
         print("\nМетод DELETE")
         result_delete = GoogleMapsApi.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        token_delete = self.get_list_token(result_delete)
+        Checking.check_json_token(result_delete, token_delete)
 
         print("\nМетод GET DELETE")
         result_get = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        token_get = self.get_list_token(result_get)
+        Checking.check_json_token(result_get, token_get)
+
+        print("\nТестирование прошло успешно!")
+    
+    def get_list_token(self, response):
+        return list(json.loads(response.text))
--- utils/checking.py
+++ utils/checking.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 """Методы для проверки ответов запросов"""
 
@@ -7,8 +8,12 @@ class Checking():
     """Метод для проверки статус-кода"""
     @staticmethod
     def check_status_code(response, status_code):
-        assert status_code == response.status_code, "Статус-код не совпадает с ожидаемым"
-        if response.status_code == status_code:
-            print(f"Успешно! Статус-код: {str(response.status_code)}")
-        else:
-            print(f"Провал! Статус-код: {str(response.status_code)}")
+        assert status_code == response.status_code, f"Ожидался статус {status_code}, получен {response.status_code}"
+        print(f"Успешно! Статус-код: {str(response.status_code)}")
+
+    """Метод для проверки наличия обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_token(response, expected_tokens):
+        tokens = list(json.loads(response.text))
+        assert tokens == expected_tokens, f"Отсутствуют ключи: {set(expected_tokens) - set(tokens)}"
+        print("Все обязательные поля присутствуют")
